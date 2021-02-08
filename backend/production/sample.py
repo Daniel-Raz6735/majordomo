88c4ff3d0b6a0a67a5d3be044b950421122b7574
Shlomo Carmi <shlomow6@gmail.com>
2021-02-08 21:30:18 +0200

    sample update

--- backend/production/sample.py
+++ backend/production/sample.py
@@ -2,8 +2,8 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-def index():
-    return "Hello World!"
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port='8080')
+    application.run(host='0.0.0.0')
